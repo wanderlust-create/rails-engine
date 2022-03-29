1018bec597e0513b7d5e03ce3ae9d6ff04bdc6af
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-28 18:57:31 -0600

    -minitial commit

---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+RSpec.describe 'Merchant API' do
+  it 'sends a list of all merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful 
+  end
+end
