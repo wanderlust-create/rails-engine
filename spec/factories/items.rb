773bd4a17fca1ce8a04e0d8cef33a33d0bd1007c
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-29 15:18:09 -0600

    feat: item factory bot

---
+++ spec/factories/items.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :item do
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.sentence(word_count: 3) }
+    unit_price { Faker::Commerce.price(range: 0..17.99) }
+    association :merchant, factory: :merchant
+  end
+end
