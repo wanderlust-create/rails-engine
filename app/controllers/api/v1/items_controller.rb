a24af2909499d5f2de07a35e9574bc465ecf9786
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-30 11:58:03 -0600

    feat: Item Serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class ItemsController < ApplicationController
       def index
-        render json: Item.all
+        render json: ItemSerializer.new(Item.where(merchant_id: params[:merchant_id]))
       end
     end
   end
---
+++ app/serializers/item_serializer.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class ItemSerializer
+  include JSONAPI::Serializer
+  attributes :name, :description, :unit_price, :merchant_id
+end
