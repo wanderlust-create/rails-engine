4596c5f0b3db0060eaa6884312e9b4b698243f5e
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-30 15:56:49 -0600

    test: create item successful status

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: %i[index show]
+      resources :items, only: %i[index show create]
       resources :merchants, only: %i[index show] do
         resources :items, controller: 'merchant_items', only: %i[index]
       end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Item API' do
 
       get '/api/v1/items'
 
-      expect(response).to be_successful
+      expect(response).to have_http_status(200)
 
       items = JSON.parse(response.body, symbolize_names: true)[:data]
 
@@ -35,6 +35,47 @@ RSpec.describe 'Item API' do
 
         expect(item[:attributes]).to have_key(:merchant_id)
         expect(item[:attributes][:merchant_id]).to be_an Integer
+      end
+    end
+  end
+
+  describe 'POST/ item' do
+    context 'given complete information' do
+      it 'will accept a request to create an item' do
+        item1_params = {
+          name: 'Sunny Disposition',
+          description: 'Rare right now',
+          unit_price: 199.76,
+          merchant_id: merchant.id
+        }
+        post '/api/v1/items', params: item1_params
+
+        expect(response).to have_http_status(201)
+        item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+        expect(item).to have_key(:id)
+        expect(item[:id]).to be_a String
+
+        expect(item).to have_key(:type)
+        expect(item[:type]).to eq('item')
+
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_a String
+        expect(item[:attributes][:name]).to eq('Sunny Disposition')
+
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a String
+        expect(item[:attributes][:description]).to eq('Rare right now')
+
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a Float
+        expect(item[:attributes][:unit_price]).to eq 199.76
+
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_an Integer
+        expect(item[:attributes][:merchant_id]).to eq(merchant.id)
+
       end
     end
   end
