23529cdc69ec8f39baceacfac96b616f72c60d42
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-30 12:41:52 -0600

    feat: item index & show with serializer

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :items, only: %i[index show]
       resources :merchants, only: %i[index show] do
         resources :items, controller: 'merchant_items', only: %i[index]
       end
     end
   end
 
-  resources :items, only: %i[index show]
+
 end
