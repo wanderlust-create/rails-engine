f9ebc147234d00323fcc190d6aa332e4b608a310
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-30 11:57:16 -0600

    Appease RuboCop routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index]
-      resources :items, only: [:index]
+      resources :merchants, only: %i[index show] do
+        resources :items, only: %i[index show]
+      end
     end
   end
 end
