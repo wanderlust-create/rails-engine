8ba5c53880bd90e8511409c7014ecd24f42b8d04
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-30 13:53:25 -0600

    test: GET all items

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -12,25 +12,29 @@ RSpec.describe 'Item API' do
 
       expect(response).to be_successful
 
-      items = JSON.parse(response.body, symbolize_names: true)
+      items = JSON.parse(response.body, symbolize_names: true)[:data]
 
       expect(items.count).to eq 5
 
       items.each do |item|
         expect(item).to have_key(:id)
-        expect(item[:id]).to be_an Integer
+        expect(item[:id]).to be_a String
 
-        expect(item).to have_key(:name)
-        expect(item[:name]).to be_an String
+        expect(item).to have_key(:type)
+        expect(item[:type]).to eq('item')
 
-        expect(item).to have_key(:description)
-        expect(item[:description]).to be_an String
+        expect(item).to have_key(:attributes)
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).to be_an String
 
-        expect(item).to have_key(:unit_price)
-        expect(item[:unit_price]).to be_an Float
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_an String
 
-        expect(item).to have_key(:merchant_id)
-        expect(item[:merchant_id]).to be_an Integer
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_an Float
+
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_an Integer
       end
     end
   end
