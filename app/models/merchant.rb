d40a510ac1c761573291ae5d86b70bc86b01d12a
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-31 15:44:49 -0600

    feat: merchant find_all

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,8 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
   has_many :items
+
+  def self.search(search_params)
+    where('name ILIKE ?', "%#{search_params[:name]}%").order(:name) if search_params[:name]
+  end
 end
