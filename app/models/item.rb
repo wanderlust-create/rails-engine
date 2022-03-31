255b7436b3a0f567ecb8ddff236f729ed07e3b48
Tamara Dowis <tamara.dowis@gmail.com>
2022-03-30 22:39:20 -0600

    refactor find item

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,16 +10,17 @@ class Item < ApplicationRecord
 
   def self.search(search_params)
     if search_params[:name]
-       where('name ILIKE ?', "%#{search_params[:name]}%").order(:name)
+      where('name ILIKE ?', "%#{search_params[:name]}%").order(:name).limit 1
 
     elsif search_params[:min_price]
-      where('unit_price > ?', search_params[:min_price].to_f).order(:unit_price)
+      where('unit_price > ?', search_params[:min_price]).order(:unit_price).limit 1
 
     elsif search_params[:max_price]
-      where('unit_price < ?', search_params[:max_price].to_f).order(:unit_price)
+      where('unit_price < ?', search_params[:max_price]).order(:unit_price).limit 1
 
-    else search_params[:min_price] && search_params[:max_price]
-      where('unit_price > ?  and unit_price < ?',search_params[:min_price].to_f, search_params[:max_price].to_f).order(:unit_price)
+    else
+      search_params[:min_price] && search_params[:max_price]
+      where('unit_price > ?  and unit_price < ?', search_params[:min_price]).limit 1
     end
   end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -93,28 +93,30 @@ RSpec.describe 'Item API' do
 end
 
 RSpec.describe 'Item Search with API' do
-
   context 'GET/ item with FIND' do
-
-    let(:merchant1) { Merchant.create!(name: 'Lama Leaping') }
-    let(:merchant2) { Merchant.create!(name: 'Monkey Moaping') }
-    let(:item1) {Item.create!(name: 'One Item', description: 'one', unit_price: 12.77, merchant_id: merchant1.id)}
-    let(:item2) {Item.create!(name: 'Two Item', description: 'two', unit_price: 23.11, merchant_id: merchant1.id)}
-    let(:item3) {Item.create!(name: 'Three Item', description: 'three', unit_price: 1.7, merchant_id: merchant2.id)}
-    let(:item4) {Item.create!(name: 'Four Item', description: 'four', unit_price: 32.77, merchant_id: merchant2.id)}
-    let(:item5) {Item.create!(name: 'Five Item', description: 'five', unit_price: 175.7, merchant_id: merchant2.id)}
-
     it 'returns a single item which matches the search term' do
+      merchant1 = Merchant.create!(name: 'Lama Leaping')
+      merchant2 = Merchant.create!(name: 'Monkey Moaping')
+      item1 = Item.create!(name: 'One Item', description: 'one', unit_price: 12.77, merchant_id: merchant1.id)
+      item2 = Item.create!(name: 'Two Item', description: 'two', unit_price: 23.11, merchant_id: merchant1.id)
+      item3 = Item.create!(name: 'Three Item', description: 'three', unit_price: 1.7, merchant_id: merchant2.id)
+      item4 = Item.create!(name: 'Four Item', description: 'four', unit_price: 32.77, merchant_id: merchant2.id)
+      item5 = Item.create!(name: 'Five Item', description: 'five', unit_price: 175.7, merchant_id: merchant2.id)
+
       search_by = { min_price: 25.00 }
 
       get '/api/v1/items/find', params: search_by
 
       expect(response).to have_http_status(200)
 
-      item = JSON.parse(response.body, symbolize_names: true)
-      # require "pry"; binding.pry
+      item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(item.count).to eq 1
+      expect(item[0][:type]).to eq('item')
+      expect(item[0][:attributes][:name]).to eq('Four Item')
+      expect(item[0][:attributes][:description]).to eq('four')
+      expect(item[0][:attributes][:unit_price]).to eq 32.77
+      expect(item[0][:attributes][:merchant_id]).to eq(merchant2.id)
     end
   end
-
-
 end
